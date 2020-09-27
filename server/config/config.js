e683972969b83fb59540877ec0d33e8b3b29d97f
faustoferrajolo <fausto.ferrajolo@tecso.coop>
2020-09-27 12:50:37 -0300

    third commit

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (process.env.NODE_ENV != 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = 'mongodb+srv://mongo-user:mongo-pass@cluster0.weiod.mongodb.net/cafe';
